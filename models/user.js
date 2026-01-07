fc3760cbcf8ec7acf136812f087cc5ce7d79b288
Abhay6830 <116333233+Abhay6830@users.noreply.github.com>
2026-01-07 20:14:19 +0530

    ,

--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  userId: Number ,
-  name: String,
-  email: String,
-  age: Number
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
