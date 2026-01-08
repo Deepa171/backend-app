0beade2dc42335ff8a8f7080474eddef2c96541b
Abhay6830 <116333233+Abhay6830@users.noreply.github.com>
2026-01-08 08:52:37 +0530

    hty

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
 const User = require("../models/user");
+const Counter = require("../models/counter");
 
 // POST: Frontend → DB
 exports.addUser = async (req, res) => {
   try {
+   const counter = await Counter.findByIdAndUpdate(
+     { _id: "userId" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+   );
+
     const user = new User({
+      userId: counter.seq,
       name: req.body.name,
       email: req.body.email,
       age: req.body.age
